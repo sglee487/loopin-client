990b2b9983bb7d47da0adb2cef1a0e7ad8e52cc9
sglee487 <sglee487@gmail.com>
2024-05-25 21:06:24 +0900

    run video with video.js

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import videojs from "video.js";
 
 const greetMsg = ref("");
 const name = ref("");
@@ -15,16 +16,49 @@ async function downloadVideo() {
   await invoke("download_video");
 }
 
+const videoPlayer = ref(null);
+
+const initPlayer = (
+  options = {
+    controls: true,
+    autoplay: true,
+    preload: "auto",
+    fluid: true,
+    sources: [
+      {
+        src: "http://localhost:8080/video/1890-151167947_small/index.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  }
+) => {
+  videoPlayer.value = videojs(videoPlayer.value, options, () => {
+    console.log("onPlayerReady"); // Use console.log instead of this.player.log
+  });
+};
+
+onMounted(() => {
+  initPlayer();
+});
+
+onBeforeUnmount(() => {
+  if (videoPlayer.value) {
+    videoPlayer.value.dispose();
+  }
+});
 </script>
 
 <template>
-
   <button @click="downloadVideo">Download Video</button>
 
   <form class="row" @submit.prevent="greet">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="submit">Greet</button>
   </form>
+
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
 
   <p>{{ greetMsg }}</p>
 </template>
