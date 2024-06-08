ee7ef8b1dc8b29a5959bc1aa71b0b4b1c7cb67c3
sglee487 <sglee487@gmail.com>
2024-06-08 16:21:43 +0900

    comment

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
 
-import { createRouter, createWebHistory } from "vue-router"; // Adjusted import statement
+import { createRouter, createWebHistory } from "vue-router";
 import timeago from "vue-timeago3";
 import VideoListVue from "./components/VideoList.vue";
 import WatchVue from "./components/Watch.vue";
@@ -12,7 +12,6 @@ const routes = [
   { path: "/watch/:uuid", name: "watch", component: WatchVue },
 ];
 
-// Create the router instance and pass the `routes` option
 const router = createRouter({
   history: createWebHistory(), // Using HTML5 history mode
   routes,
