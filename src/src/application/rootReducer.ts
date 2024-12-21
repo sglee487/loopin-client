d9f68bf8635bf3af2ff0cd9e6da4878a0b015907
sglee <sglee487@gmail.com>
2024-12-21 18:03:43 +0900

    feat: add root reducer and update store configuration

--- src/src/application/rootReducer.ts
+++ src/src/application/rootReducer.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import helloWorldReducer from '../reducers/helloWorldReducer';
+import helloWorldReducer from './reducers/helloWorldReducer';
 
 export default combineReducers({
   helloWorld: helloWorldReducer,
